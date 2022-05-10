836d47341c58dbbb30dbf02e2037c41d3f39cabf
Huh521 <huhgoo99@gmail.com>
2022-05-10 18:52:52 +0900

    4월 18일 발표용 수어 알고리즘

---
+++ app/src/main/java/org/ict/sign_language_translation/MainActivity.java
@@ -0,0 +1,38 @@
+package org.ict.sign_language_translation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatabaseReference db;
+        db = FirebaseDatabase.getInstance().getReference();
+        String sentence = "";
+
+        db.child("data").child("name").child(sentence).child("title").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+    }
+}
